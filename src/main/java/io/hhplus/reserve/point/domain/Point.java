29a4ea5b7852aa9e66c3b6953bd918f2115eef8a
hyunn12 <hyunn964@gmail.com>
2024-10-14 23:17:30 +0900

    refactor: Point entity 분리 [#7]

--- src/main/java/io/hhplus/reserve/point/domain/Point.java
+++ src/main/java/io/hhplus/reserve/point/domain/Point.java
@@ -1,24 +1,29 @@
-package io.hhplus.reserve.user.domain;
+package io.hhplus.reserve.point.domain;
 
 import io.hhplus.reserve.common.domain.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
-@Table(name = "user")
+@Table(name = "point")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User extends BaseEntity {
+public class Point extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "point_id")
+    private Long pointId;
+
     @Column(name = "user_id")
     private Long userId;
 
-    @Column(name = "user_name")
-    private String userName;
+    @Column(name = "point")
+    @ColumnDefault("0")
+    private int point;
 
 }
