502efd51e80810abaec7c173fda5e05ef306360f
hyunn <hyunn964@esjee.co.kr>
2024-10-16 11:09:24 +0900

    refactor: 누락 annotation 추가 [#8]

--- src/main/java/io/hhplus/reserve/concert/infra/ConcertJpaRepository.java
+++ src/main/java/io/hhplus/reserve/concert/infra/ConcertJpaRepository.java
@@ -3,10 +3,12 @@ package io.hhplus.reserve.concert.infra;
 import io.hhplus.reserve.concert.domain.Concert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface ConcertJpaRepository extends JpaRepository<Concert, Long> {
 
     @Query("SELECT c FROM Concert c WHERE DATE(:date) BETWEEN DATE(c.reservationStartAt) AND DATE(c.reservationEndAt)")
