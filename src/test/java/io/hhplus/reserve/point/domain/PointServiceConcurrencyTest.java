8b7c48b66db72931ccb76d533956e9933ed9f12b
hyunn12 <hyunn964@gmail.com>
2024-10-23 00:19:48 +0900

    feat: 포인트 충전 동시성 테스트 추가, 사용 테스트 수정
    [#20]

--- src/test/java/io/hhplus/reserve/point/domain/PointServiceConcurrencyTest.java
+++ src/test/java/io/hhplus/reserve/point/domain/PointServiceConcurrencyTest.java
@@ -7,10 +7,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,39 +27,57 @@ class PointServiceConcurrencyTest {
 
     @Test
     @DisplayName("동시에 포인트 사용")
-    void testUsePoint() throws InterruptedException {
-
+    void testUsePoint() {
         int threadCount = 10;
-        int requestCount = 50;
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-        CountDownLatch latch = new CountDownLatch(threadCount);
+        int initPoint = 10000;
+        int usePoint = 1000;
+        Point point = Point.createBuilder().userId(userId).point(initPoint).build();
 
-        Point point = mock(Point.class);
         when(pointRepository.getPointWithLock(userId)).thenReturn(point);
 
-        doNothing().when(point).usePoint(anyInt());
-
-        for (int i = 0; i < requestCount; i++) {
-            executorService.submit(() -> {
-                try {
-                    PointCommand.Action command = PointCommand.Action.builder().userId(userId).point(1000).build();
+        final List<CompletableFuture<Void>> futures = IntStream.range(0, threadCount)
+                .mapToObj(i -> CompletableFuture.runAsync(() -> {
+                    PointCommand.Action command = PointCommand.Action.builder()
+                            .userId(userId)
+                            .point(usePoint)
+                            .build();
                     pointService.usePoint(command);
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
+                }))
+                .toList();
 
-        latch.await();
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 
-        verify(pointRepository, times(requestCount)).getPointWithLock(userId);
-        verify(point, times(requestCount)).usePoint(anyInt());
+        verify(pointRepository, times(threadCount)).getPointWithLock(userId);
+        verify(pointRepository, times(threadCount)).savePoint(point);
+
+        assertEquals(point.getPoint(), 0);
     }
 
     @Test
     @DisplayName("동시에 포인트 충전")
     void testChargePoint() {
+        final int threadCount = 10;
+        final int chargePoint = 1000;
+        final Point point = Point.createBuilder().userId(userId).point(0).build();
 
+        when(pointRepository.getPointWithLock(userId)).thenReturn(point);
+
+        List<CompletableFuture<Void>> futures = IntStream.range(0, threadCount)
+                .mapToObj(i -> CompletableFuture.runAsync(() -> {
+                    PointCommand.Action command = PointCommand.Action.builder()
+                            .userId(userId)
+                            .point(chargePoint)
+                            .build();
+                    pointService.chargePoint(command);
+                }))
+                .toList();
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+        verify(pointRepository, times(threadCount)).getPointWithLock(userId);
+        verify(pointRepository, times(threadCount)).savePoint(point);
+
+        assertEquals(point.getPoint(), threadCount * chargePoint);
     }
 
 }
